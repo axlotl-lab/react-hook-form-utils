f01765f629d2b21b41abc8fed52c065159bda37c
Mariano Malomo <mariano.malomo@gmail.com>
2024-06-12 16:40:55 -0300

    fix custom lang usage

--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -22,11 +22,11 @@ const REACT_HOOK_UTILS_DEFAULTS: { lang: string, messages: Partial<ErrorMessages
   }
 }
 
-function getMessage(error: Errors) {
+function getMessage(error: Errors, lang?: string) {
   const message = REACT_HOOK_UTILS_DEFAULTS.messages[error]
 
   if (message) {
-    return typeof message == 'string' ? message : message[REACT_HOOK_UTILS_DEFAULTS.lang]
+    return typeof message == 'string' ? message : message[lang || REACT_HOOK_UTILS_DEFAULTS.lang]
   }
 
   return 'error';
@@ -35,12 +35,13 @@ function getMessage(error: Errors) {
 function requiredRule(message?: string): { required: string | undefined }
 function requiredRule(options?: BaseRuleOptions): { required: string | undefined }
 function requiredRule(param?: string | BaseRuleOptions) {
-  let message = getMessage('required')
+  let message: string;
 
   if (typeof param === 'string') {
     message = param;
-  } else if (typeof param === 'object') {
-    if (param.message) message = param.message;
+  } else {
+    const lang = param?.lang;
+    message = param?.message || getMessage('required', lang);
   }
 
   return {
@@ -49,58 +50,62 @@ function requiredRule(param?: string | BaseRuleOptions) {
 }
 
 function minLengthRule(value: number): { minLength: { value: number; message: string; } }
-function minLengthRule(options?: MinMaxRuleOptions): { minLength: { value: number; message: string; } }
-function minLengthRule(param?: number | MinMaxRuleOptions) {
-  let message = getMessage('minLength')
-  let value: number | undefined;
+function minLengthRule(options: MinMaxRuleOptions): { minLength: { value: number; message: string; } }
+function minLengthRule(param: number | MinMaxRuleOptions) {
+  let message: string;
+  let value: number;
 
   if (typeof param === 'number') {
     value = param;
-    message = replaceAll(message, '{{value}}', param.toString());
-  } else if (typeof param === 'object') {
-    value = param.value
-    if (param.message) message = param.message;
+    message = replaceAll(getMessage('minLength'), '{{value}}', param.toString());
+  } else {
+    const lang = param.lang;
+    value = param.value;
+    message = param.message || getMessage('minLength', lang);
   }
 
   return {
-    minLength: { value: value!, message }
+    minLength: { value, message }
   }
 }
 
 function maxLengthRule(value: number): { maxLength: { value: number; message: string; } }
-function maxLengthRule(options?: MinMaxRuleOptions): { maxLength: { value: number; message: string; } }
-function maxLengthRule(param?: number | MinMaxRuleOptions) {
-  let message = getMessage('maxLength')
-  let value: number | undefined;
+function maxLengthRule(options: MinMaxRuleOptions): { maxLength: { value: number; message: string; } }
+function maxLengthRule(param: number | MinMaxRuleOptions) {
+  let message: string;
+  let value: number;
 
   if (typeof param === 'number') {
     value = param;
-    message = replaceAll(message, '{{value}}', param.toString());
-  } else if (typeof param === 'object') {
-    value = param.value
-    if (param.message) message = param.message;
+    message = replaceAll(getMessage('maxLength'), '{{value}}', param.toString());
+  } else {
+    const lang = param.lang;
+    value = param.value;
+    message = param.message || getMessage('maxLength', lang);
   }
 
   return {
-    maxLength: { value: value!, message }
+    maxLength: { value, message }
   }
 }
 
 function patternRule(value: RegExp): { pattern: { value: RegExp; message: string; } }
-function patternRule(options?: PatternOptions): { pattern: { value: RegExp; message: string; } }
-function patternRule(param?: RegExp | PatternOptions) {
-  let message = getMessage('pattern')
-  let value: RegExp | undefined;
+function patternRule(options: PatternOptions): { pattern: { value: RegExp; message: string; } }
+function patternRule(param: RegExp | PatternOptions) {
+  let message: string;
+  let value: RegExp;
 
   if (param instanceof RegExp) {
     value = param;
-  } else if (typeof param === 'object') {
-    value = param.value
-    if (param.message) message = param.message;
+    message = getMessage('pattern');
+  } else {
+    const lang = param.lang;
+    value = param.value;
+    message = param.message || getMessage('pattern', lang);
   }
 
   return {
-    pattern: { value: value!, message }
+    pattern: { value, message }
   }
 }
 
